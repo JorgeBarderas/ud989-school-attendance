cad7111eddc258e2294082bf27ef0eca3a86ae6b
Jorge Barderas <jorgebarderas@gmail.com>
2015-04-23 23:35:28 +0200

    Improvements

--- js/app.js
+++ js/app.js
@@ -29,56 +29,118 @@
 
 /* STUDENT APPLICATION */
 $(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
+    var model = {
+        students: [
+            {
+                name: "Slappy the Frog",
+                days: []
+            },
+            {
+                name: "Lilly the Lizard",
+                days: []
+            },
+            {
+                name: "Paulrus the Walrus",
+                days: []
+            },
+            {
+                name: "Gregory the Goat",
+                days: []
+            },
+            {
+                name: "Adam the Anaconda",
+                days: []
+            }
+        ],
+        days: 12,
+        loadData: function() {
+            var that = this;
+            var attendance = JSON.parse(localStorage.attendance);
+            $.each(attendance, function() {
+                var saved = this;
+                $.each(that.students, function() {
+                    if (this.name == saved.name) {
+                        this.days = saved.days;
+                        return false; //exit inner loop
+                    }
+                });
+            });
+        }
+    }
 
-    // Count a student's missed days
-    function countMissing() {
-        $allMissed.each(function() {
-            var studentRow = $(this).parent('tr'),
-                dayChecks = $(studentRow).children('td').children('input'),
-                numMissed = 0;
+    var optopus = {
+        init: function() {
+            model.loadData();
+            attendanceGridView.init();
+        },
+        getDays: function() {
+            return model.days;
+        },
+        getStudents: function() {
+            return model.students;
+        },
+        setAttendance: function(student, $row) {
+            var name = student.name,
+                $allCheckboxes = $row.find('input');
 
-            dayChecks.each(function() {
-                if (!$(this).prop('checked')) {
+            student.days = [];
+
+            $allCheckboxes.each(function() {
+                student.days.push($(this).prop('checked'));
+            });
+            localStorage.attendance = JSON.stringify(model.students);
+            return student;
+        }
+    }
+    var attendanceGridView = {
+        init: function() {
+            this.grid = $("#attendanceGrid");
+            this.head = $("thead > tr", this.grid);
+            this.body = $("tbody", this.grid);
+            this.render();
+        },
+        render: function() {
+            // draw the columns (days)
+            this.head.append("<th class='name-col'>Student Name</th>");
+            var i=1, days=optopus.getDays();
+            for (i=1; i<=days; i++) {
+                this.head.append("<th>"+i+"</th>");
+            }
+            this.head.append("<th class='missed-col'>Days Missed-col</th>");
+
+            // draw the rows (students)
+            var students = optopus.getStudents();
+            var view = this;
+            $.each(students, function() {
+                var student = this;
+                var $row = $("<tr></tr>");
+                $row.append("<td>"+student.name+"</td>");
+                for (i=1; i<=days; i++) {
+                    var $inp = $("<td><input data-day='"+i+"' type='checkbox' /></td>")
+                    $inp.find("input").prop('checked', student.days[i-1]);
+                    $row.append($inp);
+                }
+                $row.append("<td class='missed-col'></td>");
+                $row.find("input[type='checkbox']").click(function(event) {
+                    var std = optopus.setAttendance(student,$(event.target).closest("tr"));
+                    view.countMissing(std, $(event.target).closest("tr"));
+                });
+                view.body.append($row);
+                var std = optopus.setAttendance(student, $row);
+                view.countMissing(std, $row);
+            });
+        },
+        // Count a student's missed days
+        countMissing: function (student, $row) {
+            var numMissed = 0;
+            $.each(student.days, function() {
+                if (this == false) {
                     numMissed++;
                 }
             });
-
-            $(this).text(numMissed);
-        });
+            $row.find(".missed-col").text(numMissed);
+        }
     }
-
-    // Check boxes, based on attendace records
-    $.each(attendance, function(name, days) {
-        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
-            dayChecks = $(studentRow).children('.attend-col').children('input');
-
-        dayChecks.each(function(i) {
-            $(this).prop('checked', days[i]);
-        });
-    });
-
-    // When a checkbox is clicked, update localStorage
-    $allCheckboxes.on('click', function() {
-        var studentRows = $('tbody .student'),
-            newAttendance = {};
-
-        studentRows.each(function() {
-            var name = $(this).children('.name-col').text(),
-                $allCheckboxes = $(this).children('td').children('input');
-
-            newAttendance[name] = [];
-
-            $allCheckboxes.each(function() {
-                newAttendance[name].push($(this).prop('checked'));
-            });
-        });
-
-        countMissing();
-        localStorage.attendance = JSON.stringify(newAttendance);
-    });
-
-    countMissing();
+    
+    optopus.init();
 }());
